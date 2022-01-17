de28476f05fcc62816cad4c0c112f76001fe20c6
Asadbek <97333773+asadbek1803@users.noreply.github.com>
2022-01-17 11:05:18 +0500

    Add files via upload

---
+++ Buyurtma qabul qiluvchi dastur.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jan 17 10:11:25 2022
+
+@author: User 4
+"""
+mahsulot1 =[]
+while True:
+    mahsulot = input("Mahsulot nomini kiriting:")
+    mahsulot1.append(mahsulot)
+    javob = input("Yana mahsulot qo\'shasizmi?(ha/yo\'q)")
+    if javob != 'ha':
+        break
